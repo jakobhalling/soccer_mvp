fdf2a021afc51dab4e4736fd333bffb3c598f389
Jakob Halling <hallingjakob@gmail.com>
2025-05-01 10:29:09 +0200

    asd

--- src/components/admin/MatchSchedule.tsx
+++ src/components/admin/MatchSchedule.tsx
@@ -330,11 +330,15 @@ const MatchSchedule: React.FC = () => {
               <Table {...getTableProps()}>
                 <TableHead>
                   <TableRow>
-                    {headers.map((header) => (
-                      <TableHeader {...getHeaderProps({ header })}>
-                        {header.header}
-                      </TableHeader>
-                    ))}
+                    {headers.map((header) => {
+                      const headerProps = getHeaderProps({ header });
+                      const { key, ...rest } = headerProps;
+                      return (
+                        <TableHeader key={key} {...rest}>
+                          {header.header}
+                        </TableHeader>
+                      );
+                    })}
                   </TableRow>
                 </TableHead>
                 <TableBody>
